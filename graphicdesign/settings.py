11acab2c35934f9db807a598cec783125bfa34c8
Derek <thestarvingcoder@gmail.com>
2019-10-29 21:04:11 +0000

    added cart app

--- graphicdesign/settings.py
+++ graphicdesign/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django_forms_bootstrap',
     'accounts',
     'products',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+                'cart.contexts.cart_contents'
             ],
         },
     },
@@ -125,5 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
--- graphicdesign/urls.py
+++ graphicdesign/urls.py
@@ -1,4 +1,4 @@
-"""graphicdesign URL Configuration
+"""ecommerce URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -16,8 +16,17 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from accounts import urls as urls_accounts
+from products import urls as urls_products
+from cart import urls as urls_cart
+from products.views import all_products
+from django.views import static
+from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include(urls_accounts))
+    url(r'^$', all_products, name='index'),
+    url(r'^accounts/', include(urls_accounts)),
+    url(r'^products/', include(urls_products)),
+    url(r'^cart/', include(urls_cart)),
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
