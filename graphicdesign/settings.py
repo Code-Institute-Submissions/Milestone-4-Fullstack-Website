1afe0dd298675362920c902ffd6539bd97f41efa
Derek <thestarvingcoder@gmail.com>
2019-11-13 22:20:13 +0000

    testing new static file pathway

--- graphicdesign/settings.py
+++ graphicdesign/settings.py
@@ -150,7 +150,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 STATICFILES_LOCATION = 'static'
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
-STATIC_URL = '/static/'
+STATIC_URL = 'https://%s/%s/%us-east-2/static/img/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
