909ce867a9448226d170087cf810c31a66798c83
Derek <thestarvingcoder@gmail.com>
2019-11-13 19:12:53 +0000

    edited media location to S3 bucket instead of /media to show product images

--- graphicdesign/settings.py
+++ graphicdesign/settings.py
@@ -159,7 +159,7 @@ MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'https://%s/%s/" % (us-east-2, /media/images/)'
 # ---------------------------------------------
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
